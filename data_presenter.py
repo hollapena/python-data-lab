f9f76e840062b9460c5d80dc8c7ed70ed5d6a25d
Callen Schultz <callen.schultz@gmail.com>
2021-10-04 15:32:57 -0500

    Part 2 second commit

--- data_presenter.py
+++ data_presenter.py
@@ -13,15 +13,15 @@ cupcake_invoices=open("CupcakeInvoices.csv")
 
 # find_type(cupcake_invoices)
    
-# def find_customer_total(cupcake_invoices):
-#     for invoices in cupcake_invoices:
-#          split_invoices = invoices.split(',')
-#          new_num1=float(split_invoices[3])
-#          new_num2=float(split_invoices[4])
-#          total=new_num1*new_num2
-#          print(total)
+def find_customer_total(cupcake_invoices):
+    for invoices in cupcake_invoices:
+         split_invoices = invoices.split(',')
+         new_num1=float(split_invoices[3])
+         new_num2=float(split_invoices[4])
+         total=new_num1*new_num2
+         print(total)
 
-# find_customer_total(cupcake_invoices)
+find_customer_total(cupcake_invoices)
 
 def find_overall_total(cupcake_invoices):
     new_total=0
@@ -31,10 +31,12 @@ def find_overall_total(cupcake_invoices):
         new_num2=float(split_invoices[4])
         customer_total=new_num1*new_num2
         new_total += customer_total
-    print(new_total)
+    rounded_total = round(new_total, 2)
+    print(rounded_total)
 
 find_overall_total(cupcake_invoices)
 
+cupcake_invoices.close()
 
 
 
